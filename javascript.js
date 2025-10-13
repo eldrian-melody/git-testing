0d2f8c284ef031d23998df0f29683a6739a68d58
eldrian-melody <eldrian@melody.com.tw>
2025-10-13 10:21:58 +0800

    build: test git

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// test.js
+
+// A simple function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Run the function
+console.log(greet("World"));
+
+// Display current date and time
+console.log("Current time is:", new Date().toLocaleString());
