d2e68d199b72643a5f2fa6604a3e60fb20fe74d1
eldrian-melody <eldrian@melody.com.tw>
2025-10-13 17:31:27 +0800

    feat: addTask mutation

--- mongodb/src/Task/task.resolvers.ts
+++ mongodb/src/Task/task.resolvers.ts
@@ -1,4 +1,5 @@
 import { Resolvers } from "../generated/graphql";
+import { Task } from "./task.model";
 
 const taskResolvers: Resolvers = {
   Query: {
@@ -9,6 +10,50 @@ const taskResolvers: Resolvers = {
         completed: false,
       },
     ],
+  },
+
+  // Mutation: {
+  //   // Add a new task
+  //   addTask: async (_, { input }) => {
+  //     try {
+  //       if (!input) {
+  //         throw new Error("Input is undefined");
+  //       }
+
+  //       const newTask = new Task({
+  //         title: input.title,
+  //         description: input.description ?? "",
+  //         completed: input.completed,
+  //       });
+
+  //       await newTask.save();
+  //       return newTask;
+  //     } catch (error) {
+  //       console.error("Error adding task:", error);
+  //       throw new Error("Failed to add task");
+  //     }
+  //   },
+  // },
+
+  Mutation: {
+    addTask: async (_, { input }) => {
+      try {
+        if (!input) {
+          throw new Error("Input is undefined");
+        }
+        const newTask = new Task({
+          title: input.title,
+          description: input.description ?? null,
+          completed: input.completed,
+        });
+
+        await newTask.save();
+        return newTask;
+      } catch (error) {
+        console.error("Error adding task:", error);
+        throw new Error("Failed to add task");
+      }
+    },
   },
 };
 
