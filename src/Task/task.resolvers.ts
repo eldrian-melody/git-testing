186744bf6f80d18ea7d577c731a033d785c60c94
eldrian-melody <eldrian@melody.com.tw>
2025-10-14 09:08:01 +0800

    refactor: refactor _id to id in interface

--- src/Task/task.resolvers.ts
+++ src/Task/task.resolvers.ts
@@ -4,15 +4,7 @@ import { Types } from "mongoose";
 
 const taskResolvers: Resolvers = {
   Query: {
-    tasks: async () => {
-      const tasks = await Task.find();
-      return tasks.map((task) => ({
-        id: (task._id as Types.ObjectId).toString(),
-        title: task.title,
-        description: task.description,
-        completed: task.completed,
-      }));
-    },
+    tasks: async () => Task.find(),
   },
 
   Mutation: {
@@ -29,12 +21,7 @@ const taskResolvers: Resolvers = {
 
         await newTask.save();
 
-        return {
-          id: (newTask._id as Types.ObjectId).toString(),
-          title: newTask.title,
-          description: newTask.description,
-          completed: newTask.completed,
-        };
+        return newTask;
       } catch (error) {
         console.error("Error adding task:", error);
         throw new Error("Failed to add task");
